0b93a3008b2aaeb43a88b9d726f4d7a235646fd3
Devin Veraghtert <r0932546@student.thomasmore.be>
2025-02-03 13:49:28 +0100

    start of first use case and show inventorie items on page

---
+++ IMS/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.Plugins.InMemory
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        private List<Inventory> _inventories;
+
+        public InventoryRepository()
+        {
+            _inventories = new List<Inventory>()
+            {
+                new Inventory { InventorId = 1, InventoryName = "Bike Seat", Quantity = 10, Price = 2 },
+                new Inventory { InventorId = 2, InventoryName = "Bike Body", Quantity = 10, Price = 15 },
+                new Inventory { InventorId = 3, InventoryName = "Bike Wheels", Quantity = 20, Price = 8 },
+                new Inventory { InventorId = 4, InventoryName = "Bike Pedels", Quantity = 20, Price = 1 },
+            };
+        }
+
+        public async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return await Task.FromResult(_inventories);
+            }
+
+            return _inventories.Where(i => i.InventoryName.Contains(name, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
