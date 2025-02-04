785c718af4aa0d7dd764d8d9f3ed40eaa3d59d65
Devin Veraghtert <r0932546@student.thomasmore.be>
2025-02-04 13:27:44 +0100

    remove weather files // add with inventories in products

--- IMS/IMS.CoreBusiness/Product.cs
+++ IMS/IMS.CoreBusiness/Product.cs
@@ -15,5 +15,26 @@ namespace IMS.CoreBusiness
 
         [Range(0, int.MaxValue, ErrorMessage = "Price must be greater or equal to 0.")]
         public double Price { get; set; }
+
+        public List<ProductInventory> ProductInventories { get; set; } = new List<ProductInventory>();
+        public void AddInventory(Inventory inventory)
+        {
+            if (!this.ProductInventories.Any(pi => pi.Inventory is not null && pi.Inventory.InventoryName.Equals(inventory.InventoryName)))
+            {
+                this.ProductInventories.Add(new ProductInventory
+                {
+                    InventoryId = inventory.InventoryId,
+                    Inventory = inventory,
+                    InventoryQuantity = 1,
+                    ProductId = this.ProductId,
+                    Product = this
+                });
+            }
+        }
+
+        public void RemoveInventory(ProductInventory productInventory)
+        {
+            this.ProductInventories?.Remove(productInventory);
+        }
     }
 }
