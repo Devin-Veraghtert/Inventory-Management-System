fa1783af298cebad71c37764b2d6214b457f91c7
Devin Veraghtert <r0932546@student.thomasmore.be>
2025-02-06 17:19:22 +0100

    fix validation on sell

--- IMS/IMS.WebApp/ViewModelsValidations/Sell_EnsureEnoughProductQuantityAttribute.cs
+++ IMS/IMS.WebApp/ViewModelsValidations/Sell_EnsureEnoughProductQuantityAttribute.cs
@@ -1,6 +1,26 @@
-﻿namespace IMS.WebApp.ViewModelsValidations
+﻿﻿using IMS.WebApp.ViewModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModelsValidations
 {
-    internal class Sell_EnsureEnoughProductQuantityAttribute : Attribute
+    public class Sell_EnsureEnoughProductQuantity : ValidationAttribute
     {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var sellViewModel = validationContext.ObjectInstance as SellViewModel;
+            if (sellViewModel is not null) 
+            { 
+                if (sellViewModel.Product is not null)
+                {
+                    if (sellViewModel.Product.Quantity < sellViewModel.QuantityToSell)
+                    {
+                        return new ValidationResult($"There isn't enough product. There is only {sellViewModel.Product.Quantity} in the warehouse.",
+                            new[] { validationContext.MemberName });
+                    }
+                }
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
