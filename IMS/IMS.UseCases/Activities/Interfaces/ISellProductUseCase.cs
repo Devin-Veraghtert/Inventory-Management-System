fd3fb162f1d3e54c65fc26a1c0b9bb2454bfe4f7
Devin Veraghtert <r0932546@student.thomasmore.be>
2025-02-06 16:59:26 +0100

    use case created

---
+++ IMS/IMS.UseCases/Activities/Interfaces/ISellProductUseCase.cs
@@ -0,0 +1,9 @@
+﻿using IMS.CoreBusiness;
+
+namespace IMS.UseCases.Activities.Interfaces
+{
+    public interface ISellProductUseCase
+    {
+        Task ExecteAsync(string salesOrderNumber, Product product, int quantity, string doneBy);
+    }
+}
